480da26c00ade64551550aaff2fbc13b1797faf4
Micaela Korol <micaelakorol@gmail.com>
2023-06-14 23:41:01 -0300

    update version routes

--- src/configRoutes/Index.jsx
+++ src/configRoutes/Index.jsx
@@ -1,18 +1,20 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Success from "../pages/success";
 import Error from "../pages/Error";
 
-export const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path="/" element={<App />} />
-      <Route path="/success" element={<Success />} />
-      <Route path="*" element={<Error />} />
-    </>
-  )
-);
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/success",
+    element: <Success />,
+  },
+  {
+    path: "*",
+    element: <Error />,
+  },
+]);
