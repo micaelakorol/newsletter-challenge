45afb3f5a43af2f2a88629d3a849e42135f21fd9
Micaela Korol <micaelakorol@gmail.com>
2023-06-16 16:32:24 -0300

    changes in structure

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { SectionImage } from '../styled-components/Image'
-import Desktop from '../assets/images/illustration-sign-up-desktop.svg'
-import Mobile from '../assets/images/illustration-sign-up-mobile.svg'
+import React from "react";
+import { SectionImage } from "../styled-components/Image";
+import Desktop from "../assets/images/illustration-sign-up-desktop.svg";
+import Mobile from "../assets/images/illustration-sign-up-mobile.svg";
 const Image = () => {
   return (
     <SectionImage>
-      <img src={Desktop} alt="image-desktop" className='desktop'/>
-      <img src={Mobile} alt="image-mobile" className='mobile'/>
+      <img src={Desktop} alt="image-desktop" className="desktop" />
+      <img src={Mobile} alt="image-mobile" className="mobile" />
     </SectionImage>
-  )
-}
+  );
+};
 
-export default Image
+export default Image;
